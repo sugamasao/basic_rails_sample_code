45d3c5f0fc67b56fc31cc2b92455385cbabfe10c
sugamasao <sugamasao@gmail.com>
2020-07-26 22:56:21 +0900

    ./bin/rails g model entry name body:text blog:references

---
+++ db/migrate/20200726135612_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :entries do |t|
+      t.string :name
+      t.text :body
+      t.references :blog, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
