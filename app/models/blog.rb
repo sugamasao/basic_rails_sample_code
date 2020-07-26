1deb6f677b92e228b5411e021845a97f39128ad7
sugamasao <sugamasao@gmail.com>
2020-07-27 00:11:02 +0900

    add scope sample

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,13 @@ class Blog < ApplicationRecord
   has_many :entries, dependent: :destroy
   validates :title, presence: true
   validates :title, uniqueness: true
+  scope :title_search, -> (title) { where('title like :title', title: "%#{title}%") }
+
+  # return by "all"
+  scope :notfound_scope, -> { where(title: "100000").first }
+
+  # return by nil
+  def self.notfound_method
+    where(title: "100000").first
+  end
 end
